0a173fcc2e4952129eb3ee3999f62aa1f2469544
Angus Stewart <siliconlad@protonmail.com>
2024-07-29 17:32:32 +0100

    Apply cargo fmt and clippy

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,72 +1,68 @@
 use std::fmt::Display;
 
-
 #[derive(Debug)]
 pub struct TokenizedInput {
-  tokens: Vec<String>,
+    tokens: Vec<String>,
 }
 
 impl TokenizedInput {
-  pub fn cmd(&self) -> &String {
-    &self.tokens[0]
-  }
+    pub fn cmd(&self) -> &String {
+        &self.tokens[0]
+    }
 
-  pub fn args(&self) -> Vec<String> {
-    self.tokens[1..].to_vec()
-  }
+    pub fn args(&self) -> Vec<String> {
+        self.tokens[1..].to_vec()
+    }
 }
 
-pub fn clean<'a>(input: &'a mut String) -> &'a mut String {
-  *input = input.trim().to_string();
+pub fn clean(input: &mut String) -> &mut String {
+    *input = input.trim().to_string();
 
-  if input.ends_with('\n') { input.pop(); }
-  // Replace each whitespace with a single space
-  *input = input.split_whitespace().collect::<Vec<&str>>().join(" ");
+    if input.ends_with('\n') {
+        input.pop();
+    }
+    // Replace each whitespace with a single space
+    *input = input.split_whitespace().collect::<Vec<&str>>().join(" ");
 
-  input
+    input
 }
 
 pub fn tokenize(input: &mut String) -> TokenizedInput {
-  let mut in_quotes = false;
-  let mut current_token = String::new();
-  let mut tokens = Vec::<String>::new();
+    let mut in_quotes = false;
+    let mut current_token = String::new();
+    let mut tokens = Vec::<String>::new();
 
-  let cleaned = clean(input);
+    let cleaned = clean(input);
 
-  for c in cleaned.chars() {
-    match c {
-      '\'' => {
-        if in_quotes {
-          in_quotes = false;
-        } else {
-          in_quotes = true;
+    for c in cleaned.chars() {
+        match c {
+            '\'' => {
+                in_quotes = !in_quotes;
+            }
+            ' ' => {
+                if in_quotes {
+                    current_token.push(c);
+                } else {
+                    tokens.push(current_token);
+                    current_token = String::new();
+                }
+            }
+            _ => {
+                current_token.push(c);
+            }
         }
-      }
-      ' ' => {
-        if in_quotes {
-          current_token.push(c);
-        } else {
-          tokens.push(current_token);
-          current_token = String::new();
-        }
-      }
-      _ => {
-        current_token.push(c);
-      }
     }
-  }
-  // Add last token
-  tokens.push(current_token);
+    // Add last token
+    tokens.push(current_token);
 
-  // Remove empty strings
-  tokens.retain(|x| !x.is_empty());
+    // Remove empty strings
+    tokens.retain(|x| !x.is_empty());
 
-  TokenizedInput { tokens }
+    TokenizedInput { tokens }
 }
 
-
 impl Display for TokenizedInput {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{:?}", self.tokens)
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.tokens)
+    }
 }
