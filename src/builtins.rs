5c5bfd9c96af4f7c06c0797e649b55188a603a6e
Angus Stewart <siliconlad@protonmail.com>
2024-07-29 17:32:32 +0100

    Apply cargo fmt and clippy

--- src/builtins.rs
+++ src/builtins.rs
@@ -2,12 +2,12 @@ use std::error::Error;
 
 pub fn cd(args: Vec<String>) -> Result<(), Box<dyn Error>> {
     let home = std::env::var("HOME").unwrap();
-    let path = args.get(0).map_or(home.as_str(), |s| s.as_str());
+    let path = args.first().map_or(home.as_str(), |s| s.as_str());
     let mut path = path.to_string();
 
     // Replace ~ with the home directory
     if path.starts_with("~/") {
-      path = path.replace("~", &home);
+        path = path.replace("~", &home);
     }
 
     std::env::set_current_dir(path)?;
@@ -20,7 +20,7 @@ pub fn pwd() -> Result<String, Box<dyn Error>> {
 }
 
 pub fn exit() -> Result<(), Box<dyn Error>> {
-  std::process::exit(0);
+    std::process::exit(0);
 }
 
 pub fn echo(msg: Vec<String>) -> Result<(), Box<dyn Error>> {
